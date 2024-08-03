cd170ba202de685d0074c6be85657487c29f3328
Austin Hill <Austinkinghill@gmail.com>
2024-08-02 22:37:05 -0700

    initialize repo

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import { withoutVitePlugins } from '@storybook/builder-vite';
+
+const config: StorybookConfig = {
+  stories: ['../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-actions', '@chromatic-com/storybook'],
+
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+
+  docs: {},
+
+  viteFinal: async (config) => ({
+    ...config,
+    plugins: await withoutVitePlugins(config.plugins, ['vite:dts']), // skip dts plugin
+  }),
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
+};
+export default config;
