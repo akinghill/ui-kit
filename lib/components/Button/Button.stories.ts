712f79a88a0ee436e0423401cd115b9759f7ffbd
Austin Hill <Austinkinghill@gmail.com>
2024-08-04 20:01:33 -0700

    update button stories

--- lib/components/Button/Button.stories.ts
+++ lib/components/Button/Button.stories.ts
@@ -8,6 +8,16 @@ const meta: Meta<typeof Button> = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      options: ['solid', 'outline', 'ghost', 'link'],
+      control: { type: 'radio' },
+    },
+    size: {
+      options: ['default', 'sm', 'lg'],
+      control: { type: 'radio' },
+    },
+  },
 };
 
 export default meta;
@@ -16,8 +26,8 @@ type Story = StoryObj<typeof meta>;
 
 export const Solid: Story = {
   args: {
-    children: 'Click me',
-    variant: 'default',
+    children: 'Button',
+    variant: 'solid',
     size: 'lg',
   },
 };
@@ -26,6 +36,7 @@ export const Outline: Story = {
   args: {
     variant: 'outline',
     children: 'Button',
+    size: 'lg',
   },
 };
 
@@ -33,5 +44,14 @@ export const Ghost: Story = {
   args: {
     variant: 'ghost',
     children: 'Button',
+    size: 'lg',
+  },
+};
+
+export const Link: Story = {
+  args: {
+    variant: 'link',
+    children: 'Button',
+    size: 'lg',
   },
 };
