b94f819cccca9978ca429880bbb137e1711af861
Austin Hill <Austinkinghill@gmail.com>
2024-08-06 21:52:19 -0700

    add Text stories

--- lib/components/Text/Text.stories.ts
+++ lib/components/Text/Text.stories.ts
@@ -30,8 +30,64 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    as: 'h1',
+    as: 'p',
     variant: 'body_1',
     children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+};
+
+export const Heading1: Story = {
+  args: {
+    as: 'h1',
+    variant: 'heading_1',
+    children: 'Heading 1',
+  },
+};
+
+export const Heading2: Story = {
+  args: {
+    as: 'h2',
+    variant: 'heading_2',
+    children: 'Heading 2',
+  },
+};
+
+export const Heading3: Story = {
+  args: {
+    as: 'h3',
+    variant: 'heading_3',
+    children: 'Heading 3',
+  },
+};
+
+export const Body1: Story = {
+  args: {
+    as: 'p',
+    variant: 'body_1',
+    children: 'Body 1',
+  },
+};
+
+export const Body2: Story = {
+  args: {
+    as: 'p',
+    variant: 'body_2',
+    children: 'Body 2',
+  },
+};
+
+export const Utility1: Story = {
+  args: {
+    as: 'p',
+    variant: 'utility_1',
+    children: 'Utility 1',
+  },
+};
+
+export const Utility2: Story = {
+  args: {
+    as: 'p',
+    variant: 'utility_2',
+    children: 'Utility 2',
   },
 };
